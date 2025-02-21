1d069bbcf0c9a96b9a7f3fd21da6b2697aa21fd5
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-02-21 15:08:21 +0100

    Feat:  Finish static Contact page

--- automated-aquarium-web/src/app/contact/page.tsx
+++ automated-aquarium-web/src/app/contact/page.tsx
@@ -1,21 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
 export default function Contact() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 pt-32">
-      <div
-        className="flex flex-col gap-4 items-center justify-center text-center sm:text-left"
-        style={{
-          // backgroundImage: "url('/contactBackground.jpg')",
-          // backgroundSize: "cover",
-          // backgroundPosition: "center",
-          width: "100%",
-          height: "300px",
-        }}
-      >
-        <div style={{ backgroundColor: "rgba(17, 24, 39, 0.75)" }} className="p-8">
+    <div className="flex grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 pt-32 ml-16 mr-16">
+      <main className="flex-1 flex items-center justify-center relative h-[400px] sm:h-[500px]">
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover bg-center rounded"
+          style={{ backgroundImage: "url('/contact.png')" }}
+        />
+
+        {/* Content Centered */}
+        <div className="relative z-10 text-white text-center p-8 rounded" style={{ backgroundColor: "rgba(17, 24, 39, 0.75)" }}>
           <h1 className="text-4xl font-semibold">Contact Us</h1>
         </div>
-      </div>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      </main>
+      <main className="flex-1 flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex flex-col gap-4 items-center">
           <h2 className="text-xl font-semibold">We'd love to hear from you!</h2>
           <form className="flex flex-col gap-4 w-full max-w-md">
