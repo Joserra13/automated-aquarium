a56f55fa72bf50ebcb38840f69318b2092b662b9
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-02-21 21:55:37 +0100

    Feat: Add dropdown menu for mobile view

--- automated-aquarium-web/src/components/Header.tsx
+++ automated-aquarium-web/src/components/Header.tsx
@@ -1,20 +1,28 @@
 // app/header.tsx
-
+"use client"
 // Import necessary modules
-import Link from 'next/link';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import Image from 'next/image';
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Image from "next/image";
+import { useState } from "react";
 
 // Define the Header component
 export default function Header() {
+  const [isMobileNavVisible, setIsMobileNavVisible] = useState(false);
+
   return (
     <header className="text-white py-4 sticky top-0 z-50 font-[family-name:var(--font-geist-sans)] h-32">
       {/* Header container */}
       <div className="container mx-auto px-4 flex justify-center items-center md:justify-between relative">
         {/* Website title with icon */}
         <Link href="/" className="flex items-center gap-2">
-            <Image src="/automated-aquarium/main-icon.svg" alt="Main Icon" width={32} height={32} />
-            <h1 className="text-xl font-semibold">automatedAquarium</h1>
+          <Image
+            src="/automated-aquarium/main-icon.svg"
+            alt="Main Icon"
+            width={32}
+            height={32}
+          />
+          <h1 className="text-xl font-semibold">automatedAquarium</h1>
         </Link>
         {/* Navigation menu */}
         <nav className="hidden md:block absolute left-1/2 transform -translate-x-1/2">
@@ -52,6 +60,55 @@ export default function Header() {
           <SocialIcons />
         </div>
         {/* Add Mobile Navigation Toggle Here */}
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsMobileNavVisible(!isMobileNavVisible)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <nav id="mobile-nav" className={`md:hidden absolute top-full left-0 right-0 ${isMobileNavVisible ? '' : 'hidden'} bg-gray-800 rounded-lg p-4`}>
+          <ul className="flex flex-col gap-y-4 mt-4">
+            <li>
+              <Link href="/" className="hover:text-gray-300">
+                Home
+              </Link>
+            </li>
+            {/* <li>
+              <Link href="/about" className="hover:text-gray-300">
+          About
+              </Link>
+            </li> */}
+            {/* <li>
+              <Link href="/portfolio" className="hover:text-gray-300">
+          Portfolio
+              </Link>
+            </li> */}
+            {/* <li>
+              <Link href="/blog" className="hover:text-gray-300">
+          Blog
+              </Link>
+            </li> */}
+            <li>
+              <Link href="/contact" className="hover:text-gray-300">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
