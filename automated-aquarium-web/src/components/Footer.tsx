afb0a5af673862589933ea3f997db27f1553a645
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-02-21 20:40:14 +0100

    Fix: Delete dot at the end of name

--- automated-aquarium-web/src/components/Footer.tsx
+++ automated-aquarium-web/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-center">
       <div>
         Open-source automated aquarium -- MIT Licensed -- ©{" "}
-        {new Date().getFullYear()} José Ramón Hoz.<br></br>
+        {new Date().getFullYear()} José Ramón Hoz<br></br>
         Version: {process.env.APP_VERSION} | commitHash:{" "}
         <Link
           href={`https://github.com/Joserra13/automated-aquarium/tree/${process.env.COMMIT_HASH}`}
