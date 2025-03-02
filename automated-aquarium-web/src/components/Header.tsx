8fccce2671b3e9402439f21d03e56dd35ab17c33
Joserra13 <Joserra_013@hotmail.com>
2025-03-02 11:37:26 +0100

    Feat: Add blog and about

--- automated-aquarium-web/src/components/Header.tsx
+++ automated-aquarium-web/src/components/Header.tsx
@@ -38,11 +38,11 @@ export default function Header() {
                 Streaming
               </Link>
             </li> */}
-            {/* <li>
+            <li>
               <Link href="/blog" className="hover:text-gray-300">
                 Blog
               </Link>
-            </li> */}
+            </li>
             <li>
               <Link href="/about" className="hover:text-gray-300">
                 About
@@ -100,13 +100,13 @@ export default function Header() {
                 Streaming
               </Link>
             </li> */}
-            {/* <li>
+            <li>
               <Link href="/blog" className="hover:text-gray-300">
                 Blog
               </Link>
-            </li> */}
+            </li>
             <li>
-              <Link href="/blog" className="hover:text-gray-300">
+              <Link href="/about" className="hover:text-gray-300">
                 About
               </Link>
             </li>
