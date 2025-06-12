669fed018963d5a07e41291648b63d52a617ac5c
José Ramón Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-06-12 15:39:47 +0200

    feat: add timing to read an error handling

--- src/RaspberryPi-Pico-W/Feeder/main.py
+++ src/RaspberryPi-Pico-W/Feeder/main.py
@@ -2,36 +2,57 @@
 import time
 from servo import Servo
 import utils
+import time
 
 # Create our Servo object, assigning the
 # GPIO pin connected the PWM wire of the servo
 my_servo = Servo(pin_id=28)
+dateFromLastToken = time.time()-3600
+dateFromLastQuery = time.time()-2
 
 utils.connect()
 
-print("Ask for token")
-idToken = utils.getToken()
-print(idToken)
-
-data = utils.readFirebase(idToken)
-print("count ")
-print(data["count"].get("integerValue"))
-
-print("Write values")
-utils.writeFirebase(idToken, 1, 10)
-print("Values written")
+ 
+# print("\nWrite values")
+# utils.writeFirebase(idToken, 1, 10)
+# print("Values written")
 
 while True:
-    my_servo.write(0)    # Move the servo clockwise
-    time.sleep_ms(1000)  # Wait for 1 second
     
-    my_servo.write(90)   # Set the Servo to stop
-    time.sleep_ms(1000)  
-    
-    my_servo.write(180)  # Move the servo counterclockwise
-    time.sleep_ms(1000)
+    if((time.time() - dateFromLastToken) > 3540):    
+        #Need to ask for a token
+        print("Ask for token")
+        try:
+            idToken = utils.getToken()
+            print(idToken)
+            dateFromLastToken = time.time()
+        except:
+            print(f"Error getting the token");
+        
+    if((time.time() - dateFromLastQuery) > 5):
+        #Query the value
+        try:
+            print(f"\nRead values {time.time()}")
+            data = utils.readFirebase(idToken)
+            print(f"feednow: {data["feednow"].get("booleanValue")}")
+            dateFromLastQuery = time.time()
+            
+            if data["feednow"].get("booleanValue"):
+                
+                my_servo.write(0)    # Move the servo clockwise
+                time.sleep_ms(1000)  # Wait for 1 second
 
-    my_servo.write(90)   # Set the Servo to stop
-    time.sleep_ms(1000)
-    print("Rotating...")
+                my_servo.write(90)   # Set the Servo to stop
+                time.sleep_ms(1000)  
 
+                my_servo.write(180)  # Move the servo counterclockwise
+                time.sleep_ms(1000)
+
+                my_servo.write(90)   # Set the Servo to stop
+                time.sleep_ms(1000)
+                print("Rotating...")
+                
+        except ValueError:
+            print("syntax error in JSON")
+        except:
+            print("Unknown error")
